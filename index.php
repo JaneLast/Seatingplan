b9b9172fb3afea2237ae6e891c03b47d88e8daa7
JaneLast <jane.kempton@gmail.com>
2014-09-18 21:58:46 +0100

    Got the initial code to work. Working on CSS

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Seating Plan</title>
+	<link href="stylesheet.css" rel="stylesheet" type="text/css" />
+	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<?php
+	print '<div class="container">';
+	if (isset($_REQUEST["submitted"])) {
+	
+	$seats = array("Jane", "Aaron", "Dave", "Trish", "Abi", "Sonny", "Romeo");
+	shuffle($seats);
+	
+	for($i= 0 ; $i <= 6 ; $i++) {
+	    $real_val = $i + 1;
+	    print '<div class="seat_'. $real_val .'">'. $seats[$i] .'</div>';
+	}
+	
+	print '<br /><a class="btn" href="?submitted">Randomize!</a></div>';
+	
+	} else {
+	    print '<div class="container"><a class="btn" href="?submitted">Randomize!</a></div>';
+	}
+	?>
+</body>
+</html>
