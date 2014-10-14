57232c85fd788a5a3c5a4140026618b01cb35423
JaneLast <jane.kempton@gmail.com>
2014-10-14 20:06:58 +0100

    Got the open file to work and added new documentation to readme

--- Ruby/seatingplan.rb
+++ Ruby/seatingplan.rb
@@ -1,23 +1,37 @@
 require 'yaml'
 require 'highline/import'
 require 'pry'
-
+require 'io/console'
+def multiline
+  puts " "
+  puts " "
+end
 loop do
   choose do |menu|
 
-  menu.layout = :menu_only
-    menu.choice(:open, "Open the names file") do 
-      File.open("names.yml", 'r+')
+  menu.shell  = true
+ menu.prompt = "\nPlease choose an option by typing in the number"
+  
+    menu.choice("Open the names file") do
+     puts "\nOpening the file......" 
+     `start names.yml`
+     multiline
     end
     
-    menu.choice(:load, "Randomize seating plan") do 
+    menu.choice("Randomize seating plan") do 
       
-    file = File.open('names.yml')
-    data = YAML::load(file)
-    
-    array = data.to_a
-    puts array.shuffle
+      file = File.open('names.yml')
+      data = YAML::load(file)
+      
+      array = data.to_a
+      puts "--------------------------------"
+      puts "Testing Circle Ltd Seating Plan"
+      puts "--------------------------------"
+      puts array.shuffle
+      multiline
     end
-    menu.choice(:quit, "Exit program") { exit }
-  end
+    
+    menu.choice("Exit program"){ exit }
+    end
 end
+
